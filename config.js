dec29a86cc00295b37e45c630460072f4aaf4c21
sammiratidata <zaunmanx@gmail.com>
2026-09-11 00:57:25 +0800

    Activate deployed contract address

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ window.MALLOC_CONFIG = {
   rpcUrl: "https://rpc.mainnet.chain.robinhood.com",
   explorerUrl: "https://robinhoodchain.blockscout.com",
   nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-  contractAddress: ""
+  contractAddress: "0xB32A4dD02C50f25e021e030bcd9E088D9dE1b2Ff"
 };
